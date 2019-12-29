680a0d040736a1e8d98c8d7610957303c75eb62c
wifelette <LeahSilber@gmail.com>
2019-12-28 18:49:21 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: 'Test 1', description: 'Blah Blah')
+Article.create(title: 'Test 2', description: 'Blah Blargh')
